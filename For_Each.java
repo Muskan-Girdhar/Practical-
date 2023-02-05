b0646734dd322283e73cbb360522d6ee5b225522
Muskan-Girdhar <122786280+Muskan-Girdhar@users.noreply.github.com>
2023-02-05 23:09:59 +0530

    Add files via upload

---
+++ For_Each.java
@@ -0,0 +1,13 @@
+package ControlStatement;
+
+public class For_Each {
+
+	public static void main(String[] args) {
+		
+		int a[]= {20,30,40,55,22};
+		//usind for each loop to print array
+		for(int i:a) {
+			System.out.println(i);
+		}
+
+	}}
---
+++ SwitchEg2.java
@@ -0,0 +1,29 @@
+package ControlStatement;
+
+public class SwitchEg2 {
+
+	public static void main(String[] args) {
+		char a='H';
+		switch(a){
+		case 'A':
+			System.out.println("A");
+			break;
+		
+		
+		case 'H':
+			System.out.println("Harinder");
+			break;
+		
+		
+		case 'd':
+			System.out.println("d");
+			break;
+		
+		
+		default:System.out.println("Not Match");
+		
+		}
+
+	}
+
+}
